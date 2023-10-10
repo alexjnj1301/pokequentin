1f499e87f1f9c7fa692b619e3c1d562a9e77a82c
alexjnj1301 <114980961+alexjnj1301@users.noreply.github.com>
2023-10-10 11:45:23 +0200

    initializing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import logo from './denis.png';
+import escape from './escape.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={escape} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +19,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
+      </body>
     </div>
   );
 }
